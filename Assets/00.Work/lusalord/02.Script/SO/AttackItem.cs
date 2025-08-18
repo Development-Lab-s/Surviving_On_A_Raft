7d7c63aeed811d24b783f71063fe41fff32fa83c
lusalord <lusalord63@gmail.com>
2025-08-18 23:45:25 +0900

    feat : Passive Item System

---
+++ Assets/00.Work/lusalord/02.Script/SO/AttackItem.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace _00.Work.lusalord._02.Script.SO
+{
+    [CreateAssetMenu(fileName = "AttackItem", menuName = "SO/ItemType/AttackItem")]
+    
+    public class AttackItem : ScriptableObject
+    {
+        
+    }
+}
