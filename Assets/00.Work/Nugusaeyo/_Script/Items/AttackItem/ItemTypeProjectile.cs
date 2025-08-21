5e8c85c292ce0e09b42e78307969b95343b2bc8a
Nugusaeyo <chsj0702@gmail.com>
2025-08-21 20:18:51 +0900

    Structuring AttackItemSO

---
+++ Assets/00.Work/Nugusaeyo/_Script/Items/AttackItem/ItemTypeProjectile.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public abstract class ItemTypeProjectile : TestAttackItem
+{
+    [Header("Projectile Settings")]
+    public GameObject ProjectilePrefab;
+    public string ProjectileName;
+    public float Speed;
+
+    private GameObject Projectile;
+
+    protected virtual void SpawnProjectile(Vector3 spawnPosition,TestPlayer player)
+    {
+        Projectile = Instantiate(ProjectilePrefab,
+            this.transform);
+        Projectile.name = ProjectileName;
+        Rigidbody2D projectileRb =  Projectile.GetComponent<Rigidbody2D>();
+        projectileRb.linearVelocityX = Speed * player.MoveDir.normalized.x;
+    }
+}
