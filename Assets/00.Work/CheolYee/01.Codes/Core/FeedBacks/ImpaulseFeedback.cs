fa029696e423b0114a87bba3d0c9748af453541b
CheolYee <mincheol2008@gmail.com>
2025-09-03 23:19:21 +0900

    Grenade Impulse effect
    Grenade Impulse effect

---
+++ Assets/00.Work/CheolYee/01.Codes/Core/FeedBacks/ImpaulseFeedback.cs
@@ -0,0 +1,19 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace _00.Work.CheolYee._01.Codes.Core.FeedBacks
+{
+    public class ImpulseFeedback : Feedback
+    {
+        [SerializeField] private CinemachineImpulseSource cinemachineImpulseSource;
+        [SerializeField] private float impulseForce = 0.1f;
+        public override void CreateFeedback()
+        {
+            cinemachineImpulseSource.GenerateImpulse();
+        }
+
+        public override void FinishFeedback()
+        {
+        }
+    }
+}
