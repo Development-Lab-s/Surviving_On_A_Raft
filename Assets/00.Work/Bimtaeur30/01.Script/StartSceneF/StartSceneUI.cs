cb0c8210a6031e6da799ca754ab562e8c40ba45e
Bimtaeur30 <Bimtaeur30@gmail.com>
2025-09-17 17:04:50 +0900

    aaaaaaaaaaaaaaaaa

--- Assets/00.Work/Bimtaeur30/01.Script/StartSceneF/StartSceneUI.cs
+++ Assets/00.Work/Bimtaeur30/01.Script/StartSceneF/StartSceneUI.cs
@@ -22,7 +22,7 @@ public class StartSceneUI : MonoBehaviour
         seq.Join(TitleRec.DOAnchorPos(new Vector2(0, 203), 1f));
         seq.Join(TxtRec.DOAnchorPos(new Vector2(0, 75), 1f));
         seq.Join(ButtonsRec.DOAnchorPos(new Vector2(0, -480), 1f));
-        seq.Join(TitleRec.gameObject.GetComponent<TextMeshProUGUI>().DOFade(1f, 1f));
+        seq.Join(TitleRec.gameObject.GetComponent<CanvasGroup>().DOFade(1f, 1f));
         seq.Join(TxtRec.gameObject.GetComponent<TextMeshProUGUI>().DOFade(1f, 1f));
         
         seq.OnComplete(() => StartBtn.SetActive(true));
